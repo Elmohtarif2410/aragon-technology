a555999f0a84f9398619ff5b85fb5c58776e66e0
ahmed kamal <101001511+Elmohtarif2410@users.noreply.github.com>
2022-07-07 15:27:45 +0200

    updated

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@
     website : https://elmohtarif2410.github.io/My-portofolio
     deat finsh : it's not finsh
 */
+
+/********************************************************
+################## Home page script #####################
+********************************************************/
+
+/******************* Buttons Actions ********************/
+
+// Reset Varible
+let landdingButtonMore = document.querySelectorAll(".landding .carousel-item button");
+
+landdingButtonMore.forEach((button) => {
+    button.onclick = _ => {
+        window.location.href = "/projects"
+    }
+})
