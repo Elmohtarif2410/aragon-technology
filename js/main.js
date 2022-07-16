4b0ca886326c68457b708756ba904655ddd8382f
ahmed kamal <101001511+Elmohtarif2410@users.noreply.github.com>
2022-07-16 10:10:10 +0200

    V1.11

--- js/main.js
+++ js/main.js
@@ -13,9 +13,41 @@
 
 // Reset Varible
 let landdingButtonMore = document.querySelectorAll(".landding .carousel-item button");
+let scrollToTopButton = document.getElementById("scroll_to_top");
 
+// button learn more to landding section
 landdingButtonMore.forEach((button) => {
     button.onclick = _ => {
         window.location.href = "/projects"
     }
 })
+
+// Show and hide button when windwo on scroll
+window.addEventListener("scroll", () => {
+     // function button scroll top page
+     if (window.scrollY >= 700) {
+
+        scrollToTopButton.style.display = "flex";
+
+    } else {
+
+        scrollToTopButton.style.display = "none";
+    }
+})
+
+// top page bottun on click action
+scrollToTopButton.onclick =  _ => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    })
+}
+
+
+
+
+
+
+
+
