5989c5f219f290d470eabed2bd28cf49ff6bae54
ahmed kamal <101001511+Elmohtarif2410@users.noreply.github.com>
2022-07-25 10:42:01 +0200

    content V1.5

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /*
     Template: Aragon Technology
     made by : Ahmed Elmohtarif
-    website : https://elmohtarif2410.github.io/My-portofolio
-    deat finsh : it's not finsh
+    website : muhtarif.me
+    deat finsh : 25 / 7 / 2022
 */
 
 /********************************************************
